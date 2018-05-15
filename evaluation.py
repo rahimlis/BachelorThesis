db9cd088c09a395097c80f56f6f3867d32aa5b8c
rahim <ragim95@gmail.com>
2018-05-15 16:45:50 +0300

    separated accuracy and prediction functions into different files

--- evaluation.py
+++ evaluation.py
@@ -1,13 +1,15 @@
-import operator
-
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
-import utils
-from train_network import model
 import params
+from train_network import model
 from vggish import vggish_params
-from vggish import vggish_input
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Read model and calculate accuracy on test set')
+parser.add_argument('--checkpoint', type=str, help='Trained model checkpoint')
+parser.add_argument('--test_fold', type=int, help='Number of test fold')
 
 
 def calculate_accuracy(checkpoint, test_fold):
@@ -45,52 +47,9 @@ def calculate_accuracy(checkpoint, test_fold):
         print("Train Accuracy:", train_accuracy)
 
 
-def make_prediction(wav_file, checkpoint, test_fold):
-    graph, prediction, softmax_prediction = model(False)
-
-    class_map, _, _ = utils.read_csv()
-
-    with graph.as_default(), tf.Session(graph=graph) as sess:
-        model_var_names = [v.name for v in tf.global_variables()]
-
-        model_vars = [v for v in tf.global_variables() if v.name in model_var_names]
-
-        # Use a Saver to restore just the variables selected above.
-        saver = tf.train.Saver(model_vars, name='model_load_pretrained',
-                               write_version=1)
-
-        checkpoint_path = params.CHECKPOINT_FOLDER + str(test_fold) + checkpoint
-
-        saver.restore(sess, checkpoint_path)
-
-        input_data = vggish_input.wavfile_to_examples(wav_file)
-
-        features_tensor = sess.graph.get_tensor_by_name(vggish_params.INPUT_TENSOR_NAME)
-
-        argmax_predict = sess.run(prediction, feed_dict={features_tensor: input_data})
-        top_5_predict = sess.run(softmax_prediction, feed_dict={features_tensor: input_data})
-
-        print(argmax_predict)
-
-        print("\n".join(format_top5(class_map, top_5_predict)))
-
-        return argmax_predict, top_5_predict
+if __name__ == '__main__':
+    args = parser.parse_args()
+    calculate_accuracy(**vars(args))
 
 
-def format_top5(class_map, top5):
-    result_dicts = []
-    for probabilities, predictions in zip(top5[0], top5[1]):
-        dict = {}
-        for cl, p in zip(predictions, probabilities):
-            dict[class_map[cl]] = round(p, 6)
-
-        sorted_dict = sorted(dict.items(), key=operator.itemgetter(1), reverse=True)
-
-        result_dicts.append(str(sorted_dict))
-
-    return result_dicts
-
-
-# calculate_accuracy("/checkpoint.ckpt-12500", 1)
-
-argmax, top5 = make_prediction("esc50/audio/2-119161-C-8.wav", "/checkpoint.ckpt-12500", 1)
+# argmax, top5 = make_prediction("esc50/audio/2-119161-C-8.wav", "/checkpoint.ckpt-12500", 1)
